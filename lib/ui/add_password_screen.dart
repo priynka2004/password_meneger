a9d51f50955dba3f0ddf70871a75fccdf50fea83
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2023-11-14 21:08:25 +0530

    create screen

--- lib/ui/add_password_screen.dart
+++ lib/ui/add_password_screen.dart
@@ -1,25 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:password_meneger/util/strings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AddPasswordScreen extends StatefulWidget {
-  const AddPasswordScreen({super.key});
+  const AddPasswordScreen({Key? key}) : super(key: key);
 
   @override
-  State<AddPasswordScreen> createState() => _AddPasswordScreenState();
+  AddPasswordScreenState createState() => AddPasswordScreenState();
 }
 
-class _AddPasswordScreenState extends State<AddPasswordScreen> {
-  String currentValue = 'Facebook';
-  List<String> list = ['Facebook', 'Gmail', 'Github','Telegram', 'Instagram'];
+class AddPasswordScreenState extends State<AddPasswordScreen> {
+  String currentValue = Strings.currentValue;
+  List<String> list = [
+    Strings.list,
+    Strings.list1,
+    Strings.list2,
+    Strings.list3,
+    Strings.list4,
+    Strings.list5,
+    Strings.list6,
+    Strings.list7,
+    Strings.list8,
+    Strings.list9,
+    Strings.list10,
+    Strings.list11,
+    Strings.list12,
+  ];
   String password = '';
   String email = '';
+
+  @override
+  void initState() {
+    super.initState();
+    data();
+  }
+
+  Future<void> data() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? password1 = sharedPreferences.getString('$currentValue password');
+    String? email1 = sharedPreferences.getString('$currentValue email');
+    setState(() {
+      password = password1 ?? Strings.passwordText;
+      email = email1 ?? Strings.emailText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-        ),
+        appBar: AppBar(),
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
@@ -27,23 +57,18 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
               children: [
                 DropdownButton<String>(
                   value: currentValue,
-                  onChanged: (value) async {
-                    currentValue = value!;
-                    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-                    String? password1 = sharedPreferences.getString('$currentValue password');
-                    String? email1 = sharedPreferences.getString('$currentValue email');
-                    password = password1 ?? '';
-                    email = email1 ?? '';
-                    setState(() {});
+                  onChanged: (value) {
+                    setState(() async {
+                      currentValue = value!;
+                      data();
+                    });
                   },
-                  items: list.map(
-                        (e) {
-                      return DropdownMenuItem(
-                        value: e,
-                        child: Text(e),
-                      );
-                    },
-                  ).toList(),
+                  items: list.map((e) {
+                    return DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    );
+                  }).toList(),
                 ),
                 const SizedBox(
                   height: 18,
@@ -70,4 +95,3 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
     );
   }
 }
-
--- lib/ui/save_password_screen.dart
+++ lib/ui/save_password_screen.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:password_meneger/ui/add_password_screen.dart';
+import 'package:password_meneger/util/strings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PasswordSaveScreen extends StatefulWidget {
-  const PasswordSaveScreen({super.key});
+  const PasswordSaveScreen({Key? key}) : super(key: key);
 
   @override
   State<PasswordSaveScreen> createState() => _PasswordSaveScreenState();
 }
 
 class _PasswordSaveScreenState extends State<PasswordSaveScreen> {
-  String currentValue = 'Facebook';
+  String currentValue = Strings.currentValue;
   List<String> list = [
-    'Facebook',
-    'Gmail',
-    'Github',
-    'Instagram',
-    'Telegram',
+    Strings.list,
+    Strings.list1,
+    Strings.list2,
+    Strings.list3,
+    Strings.list4,
+    Strings.list5,
+    Strings.list6,
+    Strings.list7,
+    Strings.list8,
+    Strings.list9,
+    Strings.list10,
+    Strings.list11,
+    Strings.list12,
   ];
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -29,91 +38,99 @@ class _PasswordSaveScreenState extends State<PasswordSaveScreen> {
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                DropdownButton<String>(
-                  value: currentValue,
-                  onChanged: (String? item) {
-                    passwordController.clear();
-                    currentValue = item!;
-                    setState(() {});
-                  },
-                  items: list.map(
-                    (e) {
-                      return DropdownMenuItem(
-                        value: e,
-                        child: Text(e),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  DropdownButton<String>(
+                    value: currentValue,
+                    onChanged: (String? item) {
+                      passwordController.clear();
+                      currentValue = item!;
+                      setState(() {});
+                    },
+                    items: list.map(
+                          (e) {
+                        return DropdownMenuItem(
+                          value: e,
+                          child: Text(e),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      hintText: Strings.emailText1,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      hintText: Strings.passwordText1,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(
+                        const Size(500, 50),
+                      ),
+                    ),
+                    onPressed: () async {
+                      await saveData();
+                    },
+                    child: const Text(Strings.buttonText),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                      minimumSize: MaterialStateProperty.all(const Size(500, 50)),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const AddPasswordScreen();
+                          },
+                        ),
                       );
                     },
-                  ).toList(),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    hintText: 'Enter Email',
+                    child: const Text(Strings.textButton),
                   ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    hintText: 'Enter Password',
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    String email = emailController.text;
-                    String password = passwordController.text;
-                    SharedPreferences pref =
-                        await SharedPreferences.getInstance();
-                    pref.setString('email', email);
-                    pref.setString('password', password);
-                    passwordController.clear();
-                    emailController.clear();
-                  },
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Save'),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 18,
-                ),
-                TextButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.green),
-                    minimumSize: MaterialStateProperty.all(const Size(500, 50)),
-                  ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const AddPasswordScreen();
-                    }));
-                  },
-                  child: const Text(
-                    'goto',
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> saveData() async {
+    String email = emailController.text;
+    String password = passwordController.text;
+    SharedPreferences pref =
+    await SharedPreferences.getInstance();
+    pref.setString('$currentValue email', email);
+    pref.setString('$currentValue password', password);
+    passwordController.clear();
+    emailController.clear();
   }
 }
