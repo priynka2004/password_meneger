de66251f01fd0abcab288c8faf94252215a14da1
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2023-11-14 21:03:55 +0530

    create string const class

---
+++ lib/util/strings.dart
@@ -0,0 +1,23 @@
+class Strings{
+  static const String appTitle='Flutter Demo';
+  static const String currentValue='Facebook';
+  static const String list='Facebook';
+  static const String list1='Gmail';
+  static const String list2='Github';
+  static const String list3='Telegram';
+  static const String list4='Instagram';
+  static const String list5='Windows';
+  static const String list6='macOS';
+  static const String list7='Linux';
+  static const String list8='Yahoo Mail';
+  static const String list9='Outlook';
+  static const String list10='OutFacebook';
+  static const String list11='Twitter';
+  static const String list12='LinkedIn look';
+  static const String passwordText='No Password Found';
+  static const String emailText='No Email Found';
+  static const String emailText1='Enter Email';
+  static const String passwordText1= 'Enter Password';
+  static const String buttonText= 'Save';
+  static const String textButton= 'Go to Add Password';
+}
